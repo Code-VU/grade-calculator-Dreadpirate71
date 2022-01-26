95428122d6c429de7ce6420ef7dbd9bd358fd7d0
James Remus <James.Remus@veteransunited.com>
2022-01-26 17:27:19 -0600

    steps to calculate grade

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -3,7 +3,23 @@ def calculateGrade():
     print("Calculating Grade")
     # This first line is provided for you
 
-    hrs = float(input("Enter score:"))
+    grade = input("Enter score:")
+    try:
+        float(grade)
+        if float(grade) > 1.0 or float(grade) < 0.0:
+            print ('Bad score')
+        elif float(grade) >= 0.9:
+            print ('A')
+        elif float(grade) >= 0.8 and float(grade) < 0.9:
+            print ('B')
+        elif float(grade) >= 0.7 and float(grade) < 0.8:
+            print ('C')
+        elif float(grade) >= 0.6 and float(grade) < 0.7:
+            print ('D')
+        elif float(grade) < 0.6:
+            print ('F')
+    except:
+        print ('Bad score')
 
     # end assignment
 
@@ -11,4 +27,4 @@ def calculateGrade():
 ## uncomment calculateGrade() and run > python calculateGrade.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-## calculateGrade()
+##calculateGrade()
